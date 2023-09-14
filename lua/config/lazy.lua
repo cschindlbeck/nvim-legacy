92676ca25fcd64b5398e9519bf98a2ffbd3248b0
Chris Schindlbeck <christopher.schindlbeck@iav.de>
2023-09-14 16:16:07 +0200

    Add docker, rust, tex, yaml language support

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -13,7 +13,10 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.tex" },
+    { import = "lazyvim.plugins.extras.lang.yaml" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
