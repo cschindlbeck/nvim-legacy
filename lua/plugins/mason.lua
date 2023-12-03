e29853547773dc4653042e91b9c1da9521ce8c03
Chris Schindlbeck <chris.schindlbeck@gmail.com>
2023-12-03 22:10:38 +0100

    Add all currently used Mason extras in list

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,39 @@
+return {
+
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        -- "clangd",
+        -- "cmake-language-server",
+        -- "codelldb",
+        -- "codespell",
+        -- "cpplint",
+        -- "cpptools",
+        -- "docker-compose-language-service",
+        -- "dockerfile-language-server",
+        -- "eslint-lsp",
+        -- "eslint_d",
+        "flake8",
+        -- "hadolint",
+        -- "jedi-language-server",
+        -- "json-lsp",
+        -- "llm-ls",
+        -- "lua-language-server",
+        -- "prettier",
+        -- "pyright",
+        -- "rust-analyzer",
+        "shellcheck",
+        "shfmt",
+        "stylua",
+        -- "tablo",
+        -- "texlab",
+        -- "xmlformatter",
+        -- "yaml-language-server",
+        -- "yamlfmt",
+        -- "yamllint",
+      },
+    },
+  },
+
+}
